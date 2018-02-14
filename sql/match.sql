a8b362fd40280ff97c9bb14f2cbcfdaa3dc9ee52
Briot-Ribeyre Baptiste <uapv1604137@pedago02a.univ-avignon.fr>
2018-02-14 18:58:41 +0100

    Scripts data insertion

---
+++ sql/match.sql
@@ -0,0 +1,3 @@
+DELETE FROM uapv1604137.match;
+
+INSERT INTO uapv1604137.match (id, id_team_home, id_team_visitor, date) VALUES (0, );
---
+++ sql/sport.sql
@@ -0,0 +1,5 @@
+DELETE FROM uapv1604137.sport;
+
+INSERT INTO uapv1604137.sport (id, name) VALUES (0, 'football');
+INSERT INTO uapv1604137.sport (id, name) VALUES (1, 'handball');
+INSERT INTO uapv1604137.sport (id, name) VALUES (2, 'rugby');
