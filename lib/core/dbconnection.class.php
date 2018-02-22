d1bea54f8d659d47b8c00da8169e36c60a89bcb3
uy-rrodriguez <rr.ricci@gmail.com>
2018-02-22 13:32:35 +0100

    heroku DB connection

--- lib/core/dbconnection.class.php
+++ lib/core/dbconnection.class.php
@@ -1,9 +1,37 @@
 <?php
-
+/*
 define('HOST',	'pedago02a.univ-avignon.fr');
 define('USER',	'uapv1604137');
 define('PASS',	'GeVqo1');
-define('DB',	'etd');
+define('DB',	    'etd');
+*/
+
+/*
+define('HOST',	'ec2-54-247-81-88.eu-west-1.compute.amazonaws.com');
+define('USER',	'pjuewlpereppdg');
+define('PASS',	'440a9984e16b561078f84c4f97b5ba14c99c4dc5d30ceb12010fdb7fea0e0190');
+define('DB',	'dai1mbra56rc92');
+*/
+
+$dbstr = getenv('DATABASE_URL');
+var_dump($dbstr);
+
+$dbstr = substr("$dbstr", 8);
+$dbstrarruser = explode(":", $dbstr);
+$dbstrarrhost = explode("@", $dbstrarruser[1]);
+$dbstrarrrecon = explode("?", $dbstrarrhost[1]);
+$dbstrarrport = explode("/", $dbstrarrrecon[0]);
+$dbpassword = $dbstrarrhost[0];
+$dbhost = $dbstrarrport[0];
+$dbport = $dbstrarrport[0];
+$dbuser = $dbstrarruser[0];
+$dbname = $dbstrarrport[1];
+
+define('HOST',	$dbhost);
+define('USER',	$dbuser);
+define('PASS',	$dbpassword);
+define('DB',	$dbname);
+
 
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
