f8be314b0cc74a8812a988f3122afd6ba7243a9d
uy-rrodriguez <rr.ricci@gmail.com>
2018-02-22 14:09:07 +0100

    Heroku constants.php

--- configuration/constants.php
+++ configuration/constants.php
@@ -1,5 +1,6 @@
 <?php
 //define('BASE_URL', 'http://localhost/fdsport');
 //define('BASE_URL', 'https://pedago02a.univ-avignon.fr/~uapv1604137/fdsport');
-define('BASE_URL', 'https://fdsport.herokuapp.com/fdsport.php?request=');
+define('BASE_URL', 'https://fdsport.herokuapp.com');
+define('BASE_APP_URL', 'https://fdsport.herokuapp.com/fdsport.php?request=');
 ?>
